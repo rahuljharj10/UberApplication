fd2f3af6196d4ecc0001ecb8fe02ffdd56abd6e2
RAHULRJ10 <10jharahul3@gmail.com>
2024-11-18 23:25:06 +0530

    initial commit

---
+++ src/main/java/org/rahuljhaenterprise/project/uber/uberapplication/UberApplication.java
@@ -0,0 +1,13 @@
+package org.rahuljhaenterprise.project.uber.uberapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rahuljhaenterprise/project/uber/uberapplication/UberApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rahuljhaenterprise.project.uber.uberapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
